099a989534dbd568ba15f9738ccc28886b072898
Mark A Guza <guzamaguza@gmail.com>
2019-12-15 20:53:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function findMinAndRemoveSorted(array){
 
 function merge(firstHalf, secondHalf){
     let sorted = []
-    while (arr1.length !== 0 && arr2.length !== 0) {
+    while (firstHalf.length !== 0 && secondHalf.length !== 0) {
             if (firstHalf[0] > secondHalf[0]){
                 sorted.push(findMinAndRemoveSorted(secondHalf))
             }else{
